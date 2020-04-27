944b125e101bd29d9b979de78068c1c9a892c8c3
vegarkh <vegar.hafnor@gmail.com>
2020-04-27 09:31:54 +0200

    cleaning dataframes

--- save_files.py
+++ save_files.py
@@ -4,8 +4,8 @@ from fastparquet import write, ParquetFile
 from tqdm import tqdm
 
 # set period for loading wind data
-startDate = '6/26/2016'
-endDate = '6/26/2016'
+startDate = '4/18/2018'
+endDate = '4/18/2018'
 
 # set station (place) for loading wind data
 station = '3'
@@ -16,7 +16,7 @@ dates = d.date
 
 # progress bar
 with tqdm(total=len(dates)) as pbar:
-    # fetch urls and save files locally
+    # fetch urls -> load, clean and save files on local disk
     for date in dates:
         # Package the request, send the request and catch the response: r
         r = requests.get('http://vindsiden.no/api/stations/{}?n=100&date={}'.format(station, date))
@@ -38,16 +38,44 @@ with tqdm(total=len(dates)) as pbar:
         # set 'Time' as index and delete the "Time" column
         df.set_index(time_oslo, inplace=True)
         df = df.drop("Time", axis=1)
+        df = df.sort_index()
 
         # create a subset of day (exclude night)
         day = pd.DataFrame(data=df.between_time('07:00', '22:00'))
-        print(day.head())
 
-        # convert to parquet file
-        # day_parquet = day.to_parquet('./WindData/testfile.parquet', index= True)
-        write('./WindData/spot{}date{}'.format(station, date), day)
-        df_parquet = ParquetFile('./WindData/spot{}date{}'.format(station, date)).to_pandas()
-        print(df_parquet.head())
+        time_diff = day.index.to_series().diff()
+        time_diff_mode = time_diff.mode().astype('timedelta64[m]')
+        #make a function and make it a list comprehension
+        #def timeintervall
+        #i = 0
+        #timesteps = []
+        #for i in day.index() :
+            #if day.index(i+1) - day.index(i) > 22 :
+                # i+=1
+            #elif day.index(i) == False : eller no sånt
+                #break
+            #else :
+                # timesteps.append(day.index(i+1) - day.index(i))
+                # i +=1
+        #return timeintervall.mean()
+
+        # resample data
+        day_resamp = day.resample(time_diff_mode).first()
+        print(day_resamp)
+
+        # # load wind data file from local disk
+        # winddata = ParquetFile('./WindData/spot{}'.format(station)).to_pandas()
+        #
+        # # merging dataframes together
+        # winddata = pd.concat([winddata, day], axis= 0, verify_integrity= True)
+        #
+        # # convert to parquet file
+        # write('./WindData/spot{}'.format(station), winddata)
+        #
+        # # load parquet file from local disk
+        # df_parquet = ParquetFile('./WindData/spot{}'.format(station)).to_pandas()
+        # pd.options.display.max_rows = 500
+        # print(winddata)
 
         pbar.update()
 
