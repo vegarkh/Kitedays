a775d4194a84c656801c588cc84ad4b8efc210c3
vegarkh <vegar.hafnor@gmail.com>
2020-05-06 11:29:01 +0200

    add files, make script nicer with a function call

--- loadandsave.py
+++ loadandsave.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 from fastparquet import write, ParquetFile
 from tqdm import tqdm
+from cleaning import cleaning
 from datetime import datetime
 
 # set period for loading wind data
@@ -28,86 +29,18 @@ with tqdm(total=len(dates)) as pbar:
         # make a DataFrame with columns time, wind speed and wind direction
         df = pd.DataFrame(json_data['Data'], columns=['Time', 'StationID', 'WindAvg', 'WindStDev', 'WindMax', 'WindMin',
                                                       'DirectionAvg', 'DirectionStDev', 'Temperature1'])
-        seven = str(date)+" 07:00:00"
-        seven = pd.to_datetime(seven).tz_localize('Europe/Oslo')
-        ten = str(date) + " 22:00:00"
-        ten = pd.to_datetime(ten).tz_localize('Europe/Oslo')
+        #clean and resample wind data and exclude night
+        day_resamp = cleaning(df, date, station)
 
-        #if DataFrame is empty, add rows with NaN-values at 7:00 and 22:00
-        if len(df.index) < 1:
-            df = df.append(pd.DataFrame(index=[seven])).sort_index()
-            df = df.append(pd.DataFrame(index=[ten])).sort_index()
-        else:
-            # convert the column 'Time' to datetime format
-            df['Time'] = pd.to_datetime(df['Time'])
+        # load wind data file from local disk
+        #winddata = ParquetFile('./WindData/spot{}'.format(station)).to_pandas()
 
-            # convert timezone from UTC to Europe/Oslo
-            time_utc = df['Time']
-            time_oslo = time_utc.dt.tz_convert('Europe/Oslo')
+        # merging dataframes together
+        #winddata = pd.concat([winddata, day], axis= 0, verify_integrity= True)
 
-            # set 'Time' as index
-            df.set_index(time_oslo, inplace=True)
 
-        # delete 'Time' column
-        df = df.drop("Time", axis=1)
-        df = df.sort_index()
-
-        # create a subset of day (exclude night)
-        day = pd.DataFrame(data=df.between_time('07:00', '22:00'))
-
-        # some preparing for resampling data
-        # read time for the first measurement and compare to 7:00
-        day_first = day.first_valid_index()
-        if day_first == None:
-            day_first = pd.to_datetime(seven)
-
-        #seven_am = datetime(day_first.year, day_first.month, day_first.day, 7, 0, 0)
-        #seven_am = pd.to_datetime(seven_am).tz_localize('Europe/Oslo')
-        time_gap_morning = day_first - seven
-
-        # read time for the last measurement and compare to 22:00
-        day_last = day.last_valid_index()
-        if day_last == None:
-            day_last = pd.to_datetime(ten)
-
-        #ten_pm = datetime(day_last.year, day_last.month, day_last.day, 22, 0, 0)
-        #ten_pm = pd.to_datetime(ten_pm).tz_localize('Europe/Oslo')
-        time_gap_night = ten - day_last
-
-        # the frequency of measured data is not constant
-        # find a suitable frequency: time_diff_str
-        time_diff = day.index.to_series().diff().dt.round(freq="min")
-        time_diff_mode = time_diff.mode().astype('timedelta64[m]')
-
-        if int(time_diff_mode) > 30:
-            time_diff_str = "15min"
-        else:
-            time_diff_str = str(int(time_diff_mode)) + "min"
-
-        # in case of empty or almost empty dataframe
-        # (0-1 measurements) add NaN to missing values at 7:00 and 22:00
-        if len(day.index) < 3:
-            #day = day.append(pd.DataFrame(index=[seven_am])).sort_index()
-            #day = day.append(pd.DataFrame(index=[ten_pm])).sort_index()
-            time_diff_str = "15min"
-
-        # add NaN to missing values between 7:00 and 22:00
-        if time_gap_morning.seconds/60 > int(time_diff_mode):
-            day = day.append(pd.DataFrame({'StationID': [station]}, index=[seven])).sort_index()
-        if time_gap_night.seconds/60 > int(time_diff_mode):
-            day = day.append(pd.DataFrame({'StationID': [station]}, index=[ten])).sort_index()
-        day_resamp = day.resample(time_diff_str).first()
-        day_resamp['StationID'] = station
-        print(day_resamp)
-
-        # # load wind data file from local disk
-        # winddata = ParquetFile('./WindData/spot{}'.format(station)).to_pandas()
-        #
-        # # merging dataframes together
-        # winddata = pd.concat([winddata, day], axis= 0, verify_integrity= True)
-        #
-        # # convert to parquet file
-        # write('./WindData/spot{}'.format(station), winddata)
+        # convert to parquet file
+        write('./WindData/spot{}'.format(station), day_resamp)
         #
         # # load parquet file from local disk
         # df_parquet = ParquetFile('./WindData/spot{}'.format(station)).to_pandas()
